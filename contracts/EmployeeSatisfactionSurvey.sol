e5b75b301b8e8f35d601a027304bc041191d4aca
Frank3511 <haleyzevinc5@outlook.com>
2025-11-19 09:51:27 -0800

    Optimize gas usage and clean up contract code

--- contracts/EmployeeSatisfactionSurvey.sol
+++ contracts/EmployeeSatisfactionSurvey.sol
@@ -78,27 +78,19 @@ contract EmployeeSatisfactionSurvey is SepoliaConfig {
             employeeAddress: msg.sender
         }));
 
-        // Update aggregates
-        totalRatingSum = FHE.add(totalRatingSum, rating);
-        responseCount = FHE.add(responseCount, FHE.asEuint32(1));
-
-        // Update department aggregates
-        // Note: Since FHE encrypted values cannot be directly used as mapping keys,
-        // we store all department statistics in departmentId=0 as a simplified approach.
-        // The actual departmentId is stored in the SurveyResponse struct for future reference.
-        // In production, you might need to use a different data structure that supports
-        // encrypted key lookups, or maintain separate mappings for each known department.
         euint32 one = FHE.asEuint32(1);
+        
+        totalRatingSum = FHE.add(totalRatingSum, rating);
+        responseCount = FHE.add(responseCount, one);
+
         departmentRatingSum[0] = FHE.add(departmentRatingSum[0], rating);
         departmentResponseCount[0] = FHE.add(departmentResponseCount[0], one);
 
-        // Allow contract and managers to decrypt aggregates
         FHE.allowThis(totalRatingSum);
         FHE.allowThis(responseCount);
         FHE.allowThis(departmentRatingSum[0]);
         FHE.allowThis(departmentResponseCount[0]);
         
-        // Grant access to deployer (initial manager) for decryption
         FHE.allow(totalRatingSum, deployer);
         FHE.allow(responseCount, deployer);
         FHE.allow(departmentRatingSum[0], deployer);
